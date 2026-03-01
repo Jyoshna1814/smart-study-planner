70e85248c448cc9c6f6cc0577943b16b23b70a68
Jyoshnarani <sasmaljyoshna1814@gmail.com>
2026-03-01 16:32:51 +0530

    Fixed crash error

--- server.js
+++ server.js
@@ -40,3 +40,13 @@ Daily Routine:
 
   res.json({ plan });
 });
+
+app.get("/", (req, res) => {
+  res.send("Backend is running!");
+});
+
+const PORT = process.env.PORT || 10000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
