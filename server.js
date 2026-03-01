ae842856f9daac269d651aad8a9a36826497a4c8
Jyoshnarani <sasmaljyoshna1814@gmail.com>
2026-03-01 16:14:37 +0530

    Added smart exam date logic

--- server.js
+++ server.js
@@ -8,34 +8,35 @@ app.use(express.json());
 
 app.post("/generate", (req, res) => {
   const { subject, hours, examDate } = req.body;
-  if (!subject || !hours) {
+
+  if (!subject || !hours || !examDate) {
     return res.status(400).json({ error: "Missing data" });
   }
 
- const plan = `
+  const today = new Date();
+  const exam = new Date(examDate);
+
+  const timeDifference = exam - today;
+  const daysRemaining = Math.ceil(timeDifference / (1000 * 60 * 60 * 24));
+
+  if (daysRemaining <= 0) {
+    return res.json({ plan: "Exam date must be in the future!" });
+  }
+
+  const hoursPerDay = (hours / daysRemaining).toFixed(2);
+
+  const plan = `
 Study Plan for ${subject}
 Exam Date: ${examDate}
+Days Remaining: ${daysRemaining}
+Hours Per Day: ${hoursPerDay}
 
-Hour 1:
-- Revise basic concepts
-- Watch 1 tutorial video
-- Solve 5 practice questions
-
-Remaining Hours:
-- Practice problems
-- Revise weak areas
+Daily Routine:
+- Study ${hoursPerDay} hours per day
+- Revise previous topics
+- Practice 10 questions
 - Take short notes
 `;
 
   res.json({ plan });
-});
-
-app.get("/", (req, res) => {
-  res.send("Backend is running!");
-});
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
 });
